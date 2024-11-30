fb5dbea3eac833b97a9fb96d60d5135b96f93623
shpider <3303401086@qq.com>
2024-11-30 21:43:43 +0800

    extract

--- include/ExecutorImpl.h
+++ include/ExecutorImpl.h
@@ -12,16 +12,18 @@ class ExecutorImpl final : public Executor
 {
 public:
     static ExecutorImpl* NewImpl(const Pose &my_pose= {0,0,direction::N});
+    void SetPose(const Pose &my_pose);
     ExecutorImpl();
+    explicit ExecutorImpl(Pose my_pose);
     ExecutorImpl(const ExecutorImpl&) = delete;
     ExecutorImpl& operator=(const ExecutorImpl&) = delete;
-    void Execute(const std::string &commands) noexcept;
-    Pose Query();
+    void Execute(const std::string &commands) noexcept final;
+    Pose Query() noexcept final;
 private:
-    void Move(void) noexcept;
-    void TurnLeft(void) noexcept;
-    void TurnRight(void) noexcept;
-    void Accelerate(void) noexcept;
+    void Move() noexcept;
+    void TurnLeft() noexcept;
+    void TurnRight() noexcept;
+    void Accelerate() noexcept;
     Pose pose;
     bool speed;
 };
--- src/ExecutorImpl.cpp
+++ src/ExecutorImpl.cpp
@@ -12,6 +12,19 @@ ExecutorImpl* ExecutorImpl::NewImpl(const Pose& my_pose)
     return p;
 }
 
+void ExecutorImpl::SetPose(const Pose &my_pose)
+{
+    pose = my_pose;   //C++允许结构变量整体赋值
+    /*this->pose.x = my_pose.x;
+    this->pose.y = my_pose.y;
+    this->pose.dir = my_pose.dir;*/
+}
+
+ ExecutorImpl::ExecutorImpl(Pose my_pose):pose({0,0,direction::N}), speed(false)
+ {
+     pose = my_pose;
+ }
+
 ExecutorImpl::ExecutorImpl():Executor(),pose({0,0,direction::N}),speed(false)
 {}
 
@@ -19,9 +32,19 @@ void ExecutorImpl::Execute(const std::string &commands) noexcept
 {
     for(const auto cmd:commands)
     {
-        if(cmd=='M') Move();
-        else if(cmd=='R') TurnRight();
-        else if(cmd == 'L') TurnLeft();
+        if(cmd=='M') {
+            if(speed) Move();
+            Move();
+        }
+        else if(cmd=='R') {
+            if(speed)Move();
+            TurnRight();
+        }
+        else if(cmd == 'L')
+        {
+            if(speed) Move();
+            TurnLeft();
+        }
         else if(cmd == 'F') Accelerate();
         else std::cout<<"Invalid command ";
     }
@@ -33,22 +56,22 @@ void ExecutorImpl::Move() noexcept
     pose.y += Distance[static_cast<int>(pose.dir)].y;
 }
 
-void ExecutorImpl::TurnLeft(void) noexcept
+void ExecutorImpl::TurnLeft() noexcept
 {
-
+    pose.dir = static_cast<direction>((static_cast<int>(pose.dir) + 3) % 4);
 }
 
-void ExecutorImpl::TurnRight(void) noexcept
+void ExecutorImpl::TurnRight() noexcept
 {
-
+    pose.dir = static_cast<direction>((static_cast<int>(pose.dir) + 1) % 4);
 }
 
 void ExecutorImpl::Accelerate() noexcept
 {
-
+    speed = !speed;
 }
 
-Pose ExecutorImpl::Query()
+Pose ExecutorImpl::Query() noexcept
 {
     return pose;
 }
